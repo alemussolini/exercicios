f3b12450beb614582460943b69be64de4f4f0d49
alemussolini <alessandromussolini@hotmail.com>
2026-09-10 10:39:45 -0300

    exercicio2

--- exercicios.js
+++ exercicios.js
@@ -1,8 +1,17 @@
-let resultado = soma(5 ,5)
+//let resultado = soma(5 ,5)
 
-console.log("O resultado dessa função é : " + resultado)
+//console.log("O resultado dessa função é : " + resultado)
 
-function soma(numA, numB){
-    let somatorio = numA + numB
-    return somatorio
+//function soma(numA, numB){
+//    let somatorio = numA + numB
+//    return somatorio
+//}
+
+let userName = getFirstName("Alessandro Mussolini")
+
+console.log("Welcome " + userName)
+
+function getFirstName(name){
+    let firstName = name.split(" ")[0]
+    return firstName
 }
