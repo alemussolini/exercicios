11fc6905b2e0bb3dd56417ae1cfeb9f5cc30d711
alemussolini <alessandromussolini@hotmail.com>
2026-09-10 11:01:01 -0300

    forDoWhile

---
+++ exer4.js
@@ -0,0 +1,26 @@
+//FOR
+const numeros = [1, 2, 3, 4, 5];
+
+for(let i = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
+
+//WHILE
+let jogadas = 0;
+let cara = false;
+
+while(!cara) {
+  jogadas++;
+  cara = Math.random() < 0.5;
+}
+
+console.log(`A moeda caiu cara após ${jogadas} jogadas!`);
+
+//DO-WHILE
+let numero;
+
+do {
+  numero = prompt('Digite um número entre 1 e 10:');
+} while(numero < 1 || numero > 10);
+
+console.log(`Você digitou o número ${numero}.`);
