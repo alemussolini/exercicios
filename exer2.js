60713f1a6ea0ea9af9b0be23e8b475533a7e7eaf
alemussolini <alessandromussolini@hotmail.com>
2026-09-10 10:57:10 -0300

    exercicios

---
+++ exer2.js
@@ -0,0 +1,10 @@
+//if,else if ,else
+let nivelDeFome =  5
+
+if(nivelDeFome === 1){
+console.log("pouca fome")
+}else if(nivelDeFome ===2){
+console.log("muita fome")
+}else{
+console.log("Você comeria mais que o pica pau")
+}
